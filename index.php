c4daf6ab02e1174940e56da50311991ed9790c4b
The0n1onGuy <roberto7s@hotmail.com>
2025-09-12 16:36:05 -0500

    Actualizacion sin bootstrap

--- index.php
+++ index.php
@@ -3,50 +3,35 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
     <title>PH Login</title>
     <link rel="stylesheet" href="assets/styles/app.css" />
     <script src = "/assets/scripts/app.js"></script>
 </head>
 <body class="centerC">
-    <div class="col-md-4">
-        <form class="row g-3 needs-validation p-4 border rounded-3 bg-white shadow-sm" novalidate>
-            <h2 class="text-center mb-3">Iniciar Sesión</h2>
+    <form class="Ccontainer">
+        <h2 class="centerT">Iniciar Sesión</h2>
+        <label for="validationCustom01" class="form-label">Usuario</label>
+        <input type="text" class="form-control" id="validationCustom01" required>
+        <div class="invalid-feedback">
+            Por favor, ingrese su usuario.
+        </div>
 
-            <div class="col-12">
-                <label for="validationCustom01" class="form-label">Usuario</label>
-                <input type="text" class="form-control" id="validationCustom01" required>
-                <div class="invalid-feedback">
-                    Por favor, ingrese su usuario.
-                </div>
-            </div>
-             
-            <div class="col-12">
-              <div class="row g-3 align-items-left">
-                <div class="col-auto">
-                <label for="validationCustom05" class="form-label">Contraseña</label>
-                <input type="password" class="form-control" id="validationCustom05" required>
-                <div class="invalid-feedback">
-                    Por favor, ingrese su contraseña.
-                </div>
-                </div>
-                <!-- Espacio reservado paraun boton de vista alterna de contraseña -->
-                <div class="col-auto">
-                  <button class="botonI" type="submit">Imaginate un ojo</button>
-                </div>
-                <!-------------->    
-              </div>
-            </div>
-            
-            
-            
-
-            <div class="col-12">
-                <button class="btn btn-primary w-100" type="submit">Iniciar Sesión</button>
-            </div>
-        </form>
-    </div>
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+        <label for="validationCustom05" class="form-label">Contraseña</label>
+        <input type="password" class="form-control" id="validationCustom05" required>
+        <div class="invalid-feedback">
+            Por favor, ingrese su contraseña.
+        </div>
+        <!-- Espacio reservado para un boton de vista alterna de contraseña -->
+        <button >
+                <img class="botonI" id="toggleIcon" src="assets\images\hide.svg" alt="\Proyecto_Privada\assets\images\show.svg">
+        </button>
+        
+        <div>
+            <button class="botonI" type="submit" id = "Toggle_button">Imaginate un ojo</button>
+        </div>
+        <!-------------->    
+        
+        <button class="btn btn-primary w-100" type="submit">Iniciar Sesión</button>
+    </form>
 </body>
 </html>
