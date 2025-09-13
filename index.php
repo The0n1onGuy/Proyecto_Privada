f3e1b07b01b353c00772080faaa986592cddcf97
The0n1onGuy <roberto7s@hotmail.com>
2025-09-12 19:53:10 -0500

    ultimos mods

--- index.php
+++ index.php
@@ -6,33 +6,33 @@
     <title>PH Login</title>
     <link rel="stylesheet" href="assets/styles/app.css" />
     <link rel="stylesheet" href="assets/styles/textF.css" />
+    <link rel="stylesheet" href="assets/styles/info.css" />
     <script src = "/assets/scripts/app.js"></script>
 </head>
 <body class="centerC">
-    <form>
+    <form class="flexible">
         <div class="Ccontainer">
-        <h2 class="centerT" style="padding-top: 20px;" >Iniciar Sesión</h2>
-        <label for="validationCustom01" class="form-label">Usuario</label><br/>
-        <input type="text" class="form-control" id="validationCustom01" required>
-        <div class="alerttxt">
-            <label id= "alerttxt"> Por favor, ingrese su usuario.</label>
-        </div>
+            <h2 class="centerT" style="padding-top: 20px;" >Iniciar Sesión</h2>
+            <label for="validationCustom01" class="form-label">Usuario</label><br/>
+            <div class="alerttxt">
+                <label id= "alerttxt"> Por favor, ingrese su usuario.</label>
+            </div>
+            <input type="text" class="form-control" id="validationCustom01" required><br/>        
 
-        <label for="validationCustom05" class="form-label">Contraseña</label><br/>
-        <div class="passwordC">
-            <input type="password" class="form-control" id="validationCustom05" required>
-            <button>
-                <img class="botonI" id="toggleIcon" src="assets\images\hide.svg" alt="\Proyecto_Privada\assets\images\show.svg">
-            </button>
-        </div>
-        <div class="alerttxt">
-            <label id= "alerttxt"> Por favor, ingrese su contraseña. </label>
-        </div>
-        <!-- Espacio reservado para un boton de vista alterna de contraseña -->
-        
-        <!-------------->    
-        
-        <button class="btn btn-primary w-100" type="submit">Iniciar Sesión</button>
+            <label for="validationCustom05" class="form-label">Contraseña</label><br/>
+            <div class="alerttxt">
+                <label id= "alerttxt"> Por favor, ingrese su contraseña. </label>
+            </div>
+            <div class="passwordC">
+                <input type="password" class="form-control" id="validationCustom05" required>
+                <button>
+                    <img class="botonI" id="toggleIcon" src="assets\images\show.svg" alt="mostrar/ocultar contraseña">
+                </button>
+            </div>
+            <!-- <div class="centerT">
+                <button class="btn btn-primary w-100" type="submit">Iniciar Sesión</button>
+            </div> -->
+            <button class="centerT" type="submit">Iniciar Sesión</button>
         </div>
     </form>
 </body>
