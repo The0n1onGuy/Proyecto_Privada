a3d219f4c9b54c041f3390e443f27803afa1d935
The0n1onGuy <roberto7s@hotmail.com>
2025-09-17 20:08:01 -0500

    Act con datatables

---
+++ paginas/tablaM.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>new page</title>
+    <script src="../js/jquery-3.7.1.min.js"></script>
+    <!-- <link rel="stylesheet" href="../estilos/tablaMs.css"> -->
+    <link rel="stylesheet" href="https://cdn.datatables.net/2.3.4/css/dataTables.dataTables.css" />
+    <script src="https://cdn.datatables.net/2.3.4/js/dataTables.js"></script>
+    
+</head>
+<body>
+    <script> 
+$(document).ready( function () {
+    $('#myTable').DataTable();
+} );
+    </script>
+    <table id="myTable" class="display">
+    <thead>
+        <tr>
+            <th>Column 1344</th>
+            <th>Column 2</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>Row 1 Data 1</td>
+            <td>Row 1 Data 2</td>
+        </tr>
+        <tr>
+            <td>Row 2 Data 1</td>
+            <td>Row 2 Data 2</td>
+        </tr>
+    </tbody>
+</table>
+
+<script src="../js/tablaM.js"></script>
+</body>
+</html>
