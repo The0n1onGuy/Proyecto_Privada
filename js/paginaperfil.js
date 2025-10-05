e2960b5048ffd09d2fa1c2656ff3fc682aa7174f
The0n1onGuy <roberto7s@hotmail.com>
2025-10-04 23:15:19 -0500

    041025update

--- js/paginaperfil.js
+++
@@ -1,9 +0,0 @@
-function toggleMenu() {
-      document.getElementById("sidebar").classList.toggle("active");
-    }
-
-function cerrarSesion() {
-      // Aquí se simula el cierre de sesión
-      alert("Sesión cerrada correctamente.");
-      window.location.href = "../index.php"; // Redirección al login
-    }
